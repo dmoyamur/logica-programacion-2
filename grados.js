bfceaa6e15d9b3db6d03a071eebbe7a5cf3845d3
dmoyamur <dmoya844@gmail.com>
2025-11-14 16:56:31 -0500

    JavaScript

--- grados.js
+++ grados.js
@@ -1,3 +1,16 @@
 const prompt = require("prompt-sync")();
 
-celsius = prompt(Number("Ingrese la temperatur en grados celsius: "))
+celsius = Number(prompt("Ingrese la temperatura en grados Celsius: "))
+
+function kelvin(celsius){
+    let kelvin = celsius+273.15;
+    console.log (`${celsius}°C equivalen a ${kelvin}°K`)
+}
+
+function farenheit(celsius){
+    let farenheit = celsius*1.8+32;
+    console.log (`${celsius}°C equivalen a ${farenheit}°F`)
+}
+
+kelvin(celsius);
+farenheit(celsius);
